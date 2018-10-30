883eb1b32dfdbb233092f91566da6e378376c64b
Lauren Seroka <laurenseroka@gmail.com>
2018-10-30 17:46:43 -0400

    made changes

--- heritagesites/views.py
+++ heritagesites/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from .models import HeritageSite, CountryArea
 
