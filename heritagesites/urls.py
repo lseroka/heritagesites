8bbb53c9146a727917db6abebfe9430c5462fd08
Lauren Seroka <laurenseroka@gmail.com>
2018-11-15 20:52:45 -0500

    change path name to site_filter

--- heritagesites/urls.py
+++ heritagesites/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('sites/new/', views.SiteCreateView.as_view(), name='site_new'),
 	path('sites/<int:pk>/delete/', views.SiteDeleteView.as_view(), name='site_delete'),
 	path('sites/<int:pk>/update/', views.SiteUpdateView.as_view(), name='site_update'),
-	path('search/', views.SiteFilterView.as_view(), name='search')
+	path('site_filter/', views.SiteFilterView.as_view(), name='site_filter')
 ]
