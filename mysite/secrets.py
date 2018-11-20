9bb2e81e9cb08cc11d4f19104c285c982777b711
Lauren Seroka <laurenseroka@gmail.com>
2018-11-20 17:33:01 -0500

    removing venv and scripts

---
+++ mysite/secrets.py
@@ -0,0 +1,3 @@
+SECRET_KEY = '$^e)fw8%2o9rbp@12#6rx6m_wuv_4)5rrx=89qt4&y@gx7v+n_'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '222724871792-m3ff9s435q954htend37rspasvsg1jn6.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'FSmJa33d9PRth1kruZftaACS'
